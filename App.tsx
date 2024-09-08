4c6f7b29f377025cc84a4dd48a71899c27e43eb9
Pratama <mpratamary@gmail.com>
2024-09-08 18:06:32 +0700

    fix error login

--- App.tsx
+++ App.tsx
@@ -4,13 +4,8 @@ import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./src/redux/store";
 import SignupScreen from "@/screens/SignupScreen";
-<<<<<<< HEAD
-import SplashScreen from "@/screens/SplashScreen";
-import LoginScreen from "@/screens/LoginScreen";
-=======
-import LoginScreen from "@/screens/LoginScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 import PlayScreen from "@/screens/PlayScreen";
->>>>>>> 7bc46cc5bc311f06fc7ab37fd5457652751e59b7
 
 const Stack = createNativeStackNavigator();
 
@@ -18,19 +13,6 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-<<<<<<< HEAD
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            name="login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          ></Stack.Screen>
-          <Stack.Screen
-            name="signup"
-            component={SignupScreen}
-            options={{ headerShown: false }}
-          ></Stack.Screen>
-=======
         <Stack.Navigator initialRouteName="login">
           <Stack.Screen
             name="playScreen"
@@ -45,9 +27,8 @@ export default function App() {
           <Stack.Screen
             name="login"
             component={LoginScreen}
-            options={{ headerTitle: "Login" }}
+            options={{ headerShown: false }}
           />
->>>>>>> 7bc46cc5bc311f06fc7ab37fd5457652751e59b7
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
