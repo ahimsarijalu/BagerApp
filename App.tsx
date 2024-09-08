dfbb1d51898750b807a9e9044b3fec71c4463ff1
Rijalu Ahimsa <ahimsarijalu@gmail.com>
2024-09-08 14:46:42 +0700

    adding login stack screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./src/redux/store";
 import SignupScreen from "@/screens/SignupScreen";
+import LoginScreen from "@/screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +17,12 @@ export default function App() {
             name="signup"
             component={SignupScreen}
             options={{ headerTitle: "Sign Up" }}
-          ></Stack.Screen>
+          />
+          <Stack.Screen
+            name="login"
+            component={LoginScreen}
+            options={{ headerTitle: "Login" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
